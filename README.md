c2498f012f94f8230c720b59727afe6a863e969b
jonrandoem <contact@jonrandoem.com>
2014-01-08 14:25:23 +0100

    Initial release 0.1.0

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
 yourls-semanticscuttle
 ======================
 
-A plugin for YOURLS that adds a link in the quick share box to share an URL to SemanticScuttle
+About
+------------
+
+yourls-semanticscuttle is a plugin for YOURLS. The plugin adds a new share link into the Quick Share box (available for each URL shown in the Yourls admin area).
+The added link allows the sharing of the URL to a Semantic Scuttle installation.
+
+Obviously, you need a valid Yourls installation and a valid Semantic Scuttle installation.
+
+
+
+Installation
+------------
+
+1. Unzip the plugin. You get a "yourls-semanticscuttle" folder
+2. Upload this folder into the user/plugins folder of your Yourls installation
+3. Edit your config.php file (located at user/config.php in your Yourls installation)
+4. Define the URL of your Semantic Scuttle installation by inserting the following line at the end of your config.php file:
+```
+/*
+ ** Personal settings would go after here.
+ */
+
+define('SEMANTICSCUTTLE_URL', 'https://my-semantic-scuttle.com');
+
+```
+5. Adapt the URL "https://my-semantic-scuttle.com" to your Semantic Scuttle installation's URL (No trailing slash !!!), and save changes to config.php
+6. Activate the plugin in the admin area
+
+
+
+License
+------------
+
+This theme is licensed under [MIT License](https://github.com/jonrandoem/yourls-semanticscuttle/blob/master/LICENSE).
---
+++ plugin.php
@@ -0,0 +1,51 @@
+<?php
+/**
+Plugin Name: Share on SemanticScuttle
+Plugin URI: https://github.com/jonrandoem/yourls-semanticscuttle
+Description: Add <a href="http://semanticscuttle.sourceforge.net/">SemanticScuttle</a> to the list of Quick Share destinations
+Version: 1.0
+Author: JonRandoem
+Author URI: https://github.com/jonrandoem/
+**/
+
+
+yourls_add_action( 'share_links', 'jr_yourls_sscuttle' );
+
+function jr_yourls_sscuttle( $args ) {
+	list( $longurl, $shorturl, $title, $text ) = $args;
+	$longurl = rawurlencode( $longurl );
+	$title = rawurlencode( htmlspecialchars_decode( $title ) );
+	if ( defined('SEMANTICSCUTTLE_URL') ) {
+		$sscuttle_url = SEMANTICSCUTTLE_URL;
+		echo <<<SSCUTTLE
+		
+		<style type="text/css">
+		#share_sscuttle{background:url('../user/plugins/yourls-semanticscuttle/sscuttle.gif') left center no-repeat;}
+		</style>
+		
+		<a id="share_sscuttle"
+			href="$sscuttle_url/bookmarks.php?action=add&amp;address=$longurl&amp;title=$title&amp;description="
+			title="Share on SemanticScuttle"
+			onclick="yourls_share_on_sscuttle();return false;">SemanticScuttle
+		</a>
+		
+		<script type="text/javascript">
+		// Send to SemanticScuttle open window
+		function yourls_share_on_sscuttle() {
+			var url = $('#share_sscuttle').attr('href');
+			window.open(url, 'sscuttle', 'toolbar=no,width=800,height=600');
+			return false;
+		}
+		// Dynamically update SemanticScuttle link
+		// when user clicks on the "Share" Action icon, event $('#tweet_body').keypress() is fired, so we'll add to this
+		$('#tweet_body').keypress(function(){
+			var title = encodeURIComponent( $('#titlelink').val() );
+			var url = encodeURIComponent( $('#copylink').val() );
+			var sscuttle = '$sscuttle_url/bookmarks.php?action=add&address='+url+'&title='+title+'&description=';
+			$('#share_sscuttle').attr('href', sscuttle);		
+		});
+		</script>
+
+SSCUTTLE;
+	}
+}
